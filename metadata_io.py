99f198d767ceea6c53f74edb5d1b23669fcd13bf
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2022-02-16 17:39:29 +0100

    add example to read/write metadata in an image

---
+++ metadata_io.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 15:57:35 2022
+
+@author: LSM User
+"""
+
+import json
+import numpy
+import tifffile  # http://www.lfd.uci.edu/~gohlke/code/tifffile.py.html
+
+# stolen here: https://stackoverflow.com/questions/20529187/what-is-the-best-way-to-save-image-metadata-alongside-a-tif
+
+#generates mock data and metadata
+data = numpy.arange(256).reshape((16, 16)).astype('u1')
+
+metadata = dict(microscope='zeiss', shape=data.shape, dtype=data.dtype.str, pixel_size= 0.12,pixel_size_unit="um")
+print(data.shape, data.dtype, metadata['microscope'])
+
+# saves
+metadata = json.dumps(metadata)
+tifffile.imwrite('microscope.tif', data, description=metadata)
+
+# loads
+tif = tifffile.TiffFile('microscope.tif')    
+output_metadata = tif.pages[0].tags["ImageDescription"].value
+print(output_metadata)
